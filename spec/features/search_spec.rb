678ebaad9b728cb961a9a1b9a851b70a557ad37d
Chris Cullinane <chriscullinane@protonmail.com>
2023-09-08 08:48:01 -0800

    more

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe 'Avatar Search' do
     expect(current_path).to eq(search_path)
     save_and_open_page
     expect(page).to have_content('Total People in Fire Nation:')
+    expect(page).to have_content('Azula')
+    expect(page).to have_content('Allies: Ozai, Zuko')
+    expect(page).to have_content('Enemies: Iroh, Zuko, Kuei, Long Feng, Mai, Ty Lee, Ursa')
+    expect(page).to have_content("Affiliations: Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
   end
 end
